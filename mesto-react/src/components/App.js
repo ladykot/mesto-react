1950966f72c8c8a2dbb8295e1b36deedd2908d6b
Katya Akilina <katyaartline@gmail.com>
2022-08-07 02:34:51 +0300

    feat: add closeAllPopups

--- mesto-react/src/components/App.js
+++ mesto-react/src/components/App.js
@@ -7,25 +7,30 @@ import Main from "./Main";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 
-
 function App() {
-
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
 
-  // меняем состояние
+  // состояния открытия
   const handleEditProfileClick = () => {
     setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
   };
 
   const handleEditAvatarClick = () => {
-    setIsEditAvatarPopupOpen(!isEditAvatarPopupOpen)
+    setIsEditAvatarPopupOpen(!isEditAvatarPopupOpen);
   };
 
   const handleAddPlaceClick = () => {
     setIsAddPlacePopupOpen(!isAddPlacePopupOpen);
   };
+
+  // состояния закрытия
+  const closeAllPopups = () => {
+    setIsEditProfilePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+  }
 
   return (
     <div className="page">
@@ -37,7 +42,12 @@ function App() {
       />
       <Footer />
       <ImagePopup />
-      <PopupWithForm title="Редактировать профиль" name="edit-profile" isOpen={isEditProfilePopupOpen}>
+      <PopupWithForm
+        title="Редактировать профиль"
+        name="edit-profile"
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
+      >
         <fieldset className="popup__inputs">
           <input
             type="text"
@@ -63,44 +73,52 @@ function App() {
           <span className="description-input-error popup__inputs-error"></span>
         </fieldset>
       </PopupWithForm>
-      <PopupWithForm title="Новое место" name="create-card" isOpen={isAddPlacePopupOpen}>
+      <PopupWithForm
+        title="Новое место"
+        name="create-card"
+        isOpen={isAddPlacePopupOpen}
+      >
         {/* <fieldset className="popup__inputs"> */}
-          <input
-            type="text"
-            id="title-input"
-            name="name"
-            placeholder="Название"
-            className="popup__inputs-item popup__inputs-item_type_title"
-            required
-            minlength="2"
-            maxlength="30"
-          />
-          <span className="title-input-error popup__inputs-error"></span>
-          <input
-            type="url"
-            id="link-input"
-            name="link"
-            placeholder="Ссылка на картинку"
-            className="popup__inputs-item popup__inputs-item_type_link"
-            required
-          />
-          <span className="popup__inputs-error link-input-error"></span>
+        <input
+          type="text"
+          id="title-input"
+          name="name"
+          placeholder="Название"
+          className="popup__inputs-item popup__inputs-item_type_title"
+          required
+          minlength="2"
+          maxlength="30"
+        />
+        <span className="title-input-error popup__inputs-error"></span>
+        <input
+          type="url"
+          id="link-input"
+          name="link"
+          placeholder="Ссылка на картинку"
+          className="popup__inputs-item popup__inputs-item_type_link"
+          required
+        />
+        <span className="popup__inputs-error link-input-error"></span>
         {/* </fieldset> */}
       </PopupWithForm>
       <PopupWithForm title="Вы уверены?" name="delete-card" isOpen={false}>
         {/* <fieldset className="popup__inputs"></fieldset> */}
       </PopupWithForm>
-      <PopupWithForm title="Обновить аватар" name="change-avatar" isOpen={isEditAvatarPopupOpen}>
+      <PopupWithForm
+        title="Обновить аватар"
+        name="change-avatar"
+        isOpen={isEditAvatarPopupOpen}
+      >
         {/* <fieldset className="popup__inputs"> */}
-          <input
-            type="url"
-            id="link-input-avatar"
-            name="link"
-            placeholder="Ссылка на картинку"
-            className="popup__inputs-item popup__inputs-item_type_link"
-            required
-          />
-          <span className="popup__inputs-error link-input-avatar-error"></span>
+        <input
+          type="url"
+          id="link-input-avatar"
+          name="link"
+          placeholder="Ссылка на картинку"
+          className="popup__inputs-item popup__inputs-item_type_link"
+          required
+        />
+        <span className="popup__inputs-error link-input-avatar-error"></span>
         {/* </fieldset> */}
       </PopupWithForm>
     </div>
--- mesto-react/src/components/PopupWithForm.js
+++ mesto-react/src/components/PopupWithForm.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React from "react";
 
-function PopupWithForm({title, name, children, isOpen}) {
-
+function PopupWithForm({ title, name, children, isOpen, onClose }) {
   // если переменная isOpen == true , то ставим класс 'popup_opened'
   return (
-      <section className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
+    <section
+      className={`popup popup_type_${name} ${isOpen ? "popup_opened" : ""}`}
+    >
       <div className={`popup__container popup__container_${name}`}>
-        <button type="button" className="popup__button-close popup__button-close_type_edit hover"></button>
+        <button
+          onClick={onClose}
+          type="button"
+          className="popup__button-close popup__button-close_type_edit hover"
+        ></button>
         <form name={name} className="popup__form" novalidate>
           <h3 className="popup__title">{title}</h3>
-          <fieldset className="popup__inputs">
-            {children}
-          </fieldset>
-          <button type="submit" className="popup__button-save" aria-label="Сохранить">Сохранить</button>
+          <fieldset className="popup__inputs">{children}</fieldset>
+          <button
+            
+            type="submit"
+            className="popup__button-save"
+            aria-label="Сохранить"
+          >
+            Сохранить
+          </button>
         </form>
       </div>
     </section>
-  )
+  );
 }
 
 export default PopupWithForm;
